617721806ffcefd924a86b85ef92f6e0801af973
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-07-20 12:46:32 -0300

    atualizada as rotas de categoria

--- src/controllers/CategoriaController.ts
+++ src/controllers/CategoriaController.ts
@@ -4,22 +4,22 @@ import Categoria from '../schemas/Categoria'
 
 dotenv.config()
 class CategoriaController {
-  public async get (req: Request, res: Response): Promise<Response> {
+  public async getTodas (req: Request, res: Response): Promise<Response> {
     try {
       const categoria = await Categoria.find()
       return res.status(200).json(categoria)
     } catch (err) {
-      return res.status(400).json({ mensagem: 'Usuário não encontrado', erro: err })
+      return res.status(400).json({ mensagem: 'Categoria não encontrada', erro: err })
     }
   }
 
-  public async getId (req: Request, res: Response): Promise<Response> {
+  public async get (req: Request, res: Response): Promise<Response> {
     try {
       const id = req.path.split('/').pop()
       const categoria = await Categoria.findOne({ _id: id })
       return res.status(200).json(categoria)
     } catch (err) {
-      return res.status(400).json({ mensagem: 'Usuário não encontrado', erro: err })
+      return res.status(400).json({ mensagem: 'Categoria não encontrada', erro: err })
     }
   }
 
@@ -29,7 +29,7 @@ class CategoriaController {
       const categoria = await Categoria.replaceOne({ _id: id }, req.body)
       return res.status(201).json(categoria)
     } catch (err) {
-      return res.status(400).json({ mensagem: 'Usuário não criado', erro: err })
+      return res.status(400).json({ mensagem: 'Categoria não criada', erro: err })
     }
   }
 
@@ -38,17 +38,17 @@ class CategoriaController {
       const categoria = await Categoria.create(req.body)
       return res.status(201).json(categoria)
     } catch (err) {
-      return res.status(400).json({ mensagem: 'Usuário não criado', erro: err })
+      return res.status(400).json({ mensagem: 'Categoria não criada', erro: err })
     }
   }
 
-  public async deleteId (req: Request, res: Response): Promise<Response> {
+  public async delete (req: Request, res: Response): Promise<Response> {
     try {
       const id = req.path.split('/').pop()
       const categoria = await Categoria.deleteOne({ _id: id })
       return res.status(200).json({ mensagem: 'Categoria deletada', data: categoria })
     } catch (err) {
-      return res.status(400).json({ mensagem: 'Produto não encontrado', erro: err })
+      return res.status(400).json({ mensagem: 'Categoria não encontrada', erro: err })
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -31,10 +31,10 @@ routes.get('/produto/:id', ProdutoController.getId)
 routes.delete('/produto/:id', auth.verificaToken, ProdutoController.deleteId)
 
 // Categoria
-routes.post('/categoria/cadastra', auth.verificaToken, CategoriaController.cadastra)
-routes.put('/categoria/atualiza/:id', auth.verificaToken, CategoriaController.atualiza)
-routes.get('/categoria/:id', CategoriaController.getId)
-routes.get('/categoria', CategoriaController.get)
-routes.delete('/categoria/:id', auth.verificaToken, CategoriaController.deleteId)
+routes.post('/categoria', auth.verificaToken, CategoriaController.cadastra)
+routes.put('/categoria/:id', auth.verificaToken, CategoriaController.atualiza)
+routes.get('/categoria', CategoriaController.getTodas)
+routes.get('/categoria/:id', CategoriaController.get)
+routes.delete('/categoria/:id', auth.verificaToken, CategoriaController.delete)
 
 export default routes
