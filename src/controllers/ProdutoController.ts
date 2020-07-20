dbc7ecd736f6de5c3cbff55d851bbb8cb6a4c54d
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-07-20 19:23:13 -0300

    provisoriamente tirada a exclusão de imagem do produto

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -7,10 +7,6 @@ class ProdutoController {
   // Cadastra produto
   public async cadastra (req: Request, res: Response): Promise<Response> {
     try {
-      const reqFiles = []
-      for (let i = 0; i < req.files.length; i++) {
-        reqFiles.push(req.files[i].filename)
-      }
       const data = {
         titulo: req.body.titulo,
         descricao: req.body.descricao,
@@ -19,7 +15,7 @@ class ProdutoController {
         categoria: req.body.categoria,
         destaque: req.body.destaque,
         preco: req.body.preco,
-        imagens: reqFiles
+        imagens: req.files
       }
       const produto = await Produto.create(data)
       return res.status(201).json({ produto, arquivo: req.file })
@@ -31,10 +27,6 @@ class ProdutoController {
   public async atualiza (req: Request, res: Response): Promise<Response> {
     try {
       const id = req.path.split('/').pop()
-      const reqFiles = []
-      for (let i = 0; i < req.files.length; i++) {
-        reqFiles.push(req.files[i].filename)
-      }
       const data = {
         titulo: req.body.titulo,
         descricao: req.body.descricao,
@@ -43,7 +35,7 @@ class ProdutoController {
         categoria: req.body.categoria,
         destaque: req.body.destaque,
         preco: req.body.preco,
-        imagens: reqFiles
+        imagens: req.files
       }
       const produto = await Produto.replaceOne({ _id: id }, data)
       return res.status(201).json({ produto, arquivo: req.file })
@@ -76,8 +68,8 @@ class ProdutoController {
   public async delete (req: Request, res: Response): Promise<Response> {
     try {
       const id = req.path.split('/').pop()
-      const produto = await Produto.findOne({ _id: id })
-      const deletaImagem = produto?.imagens?.forEach((valor:string) => {
+      // const produto = await Produto.findOne({ _id: id })
+      /* const deletaImagem = produto?.imagens?.forEach((valor:string) => {
         const nomeImagem = valor.split('.')
         console.log('Nome Imagem: ', nomeImagem)
         fs.unlink(`${process.cwd()}/uploads/produtos/${nomeImagem[0]}.jpg`, (err) => {
@@ -86,8 +78,7 @@ class ProdutoController {
             return res.status(400).json({ mensagem: 'Produto não deletado', erro: err })
           }
         })
-      })
-      console.log(deletaImagem)
+      }) */
       const produtoDeleta = await Produto.deleteOne({ _id: id })
       return res.status(200).json({ mensagem: 'Produto deletado', data: produtoDeleta })
     } catch (err) {
